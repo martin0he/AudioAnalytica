b8be2f0505ae551ce2ad6f5d98e1e11e1e4bd5d6
martin0he <martin.hema.mh@gmail.com>
2024-07-19 22:36:17 +0200

    addone

--- frontend/src/components/HomePage/UserSavedAlbumsGrid.tsx
+++ frontend/src/components/HomePage/UserSavedAlbumsGrid.tsx
@@ -1,4 +1,11 @@
-import { Box, Grid, keyframes, Tooltip, Typography } from "@mui/material";
+import {
+  Box,
+  CircularProgress,
+  Grid,
+  keyframes,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { styled } from "@mui/system";
 import { AlbumObject } from "../../hooks/useSavedAlbums";
@@ -84,7 +91,7 @@ const UserSavedAlbumsGrid = ({ albums }: UserFollowingArtistsGridProps) => {
             alignItems="flex-start"
             justifyContent="flex-start"
           >
-            <Typography>N/A</Typography>
+            <CircularProgress color="primary" />
           </Box>
         )}
       </ScrollableContent>
