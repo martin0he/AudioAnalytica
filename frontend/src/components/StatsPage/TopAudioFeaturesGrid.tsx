3f18cb1f9d9c19a77a89b51d7d9ada22068722a6
martin0he <martin.hema.mh@gmail.com>
2024-07-20 10:40:51 +0200

    new avg grid

--- frontend/src/components/StatsPage/TopAudioFeaturesGrid.tsx
+++ frontend/src/components/StatsPage/TopAudioFeaturesGrid.tsx
@@ -5,32 +5,75 @@ interface TopAudioFeaturesGridProps {
   audioFeatures: Feature[];
 }
 
-interface Averages {
-  [key: string]: number;
-}
-
 const TopAudioFeaturesGrid = ({ audioFeatures }: TopAudioFeaturesGridProps) => {
-  const averages = audioFeatures.reduce<Averages>((acc, feature) => {
-    Object.keys(feature).forEach((key) => {
-      if (key !== "id") {
-        const value = feature[key as keyof Feature];
-        if (typeof value === "number") {
-          acc[key] = (acc[key] || 0) + value / audioFeatures.length;
-        }
-      }
-    });
-    return acc;
-  }, {});
+  const averageFeatureValue = (values: number[]) => {
+    const sum = values.reduce((acc, curr) => acc + curr, 0);
+    return sum / values.length;
+  };
+
+  const featureNames = [
+    "Acousticness",
+    "Danceability",
+    "Energy",
+    "Instrumentalness",
+    "Liveness",
+    "Speechiness",
+    "Valence",
+    "Tempo",
+    "Duration",
+  ];
+
+  const allAcousticness = audioFeatures.map((feature) => feature.acousticness);
+  const allDanceability = audioFeatures.map((feature) => feature.danceability);
+  const allEnergy = audioFeatures.map((feature) => feature.energy);
+  const allInstrumentalness = audioFeatures.map(
+    (feature) => feature.instrumentalness
+  );
+  const allLiveness = audioFeatures.map((feature) => feature.liveness);
+  const allSpeechiness = audioFeatures.map((feature) => feature.speechiness);
+  const allValence = audioFeatures.map((feature) => feature.valence);
+  const allTempo = audioFeatures.map((feature) => feature.tempo);
+  const allDuration = audioFeatures.map((feature) => feature.duration_ms);
+
+  const convertToMinutes = (ms: number) => {
+    const minutes = Math.floor(ms / 60000);
+    const seconds = Number(((ms % 60000) / 1000).toFixed(0));
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
+
+  const allFeatureArrays = [
+    allAcousticness,
+    allDanceability,
+    allEnergy,
+    allInstrumentalness,
+    allLiveness,
+    allSpeechiness,
+    allValence,
+  ];
 
   return (
     <Box display="flex" flexDirection="column" rowGap="5px">
       <Typography>Average Audio Features</Typography>
       <Grid container spacing={1} padding="10px">
-        {Object.entries(averages).map(([key, value], index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <Typography>{`${key}: ${value.toFixed(3)}`}</Typography>
+        {allFeatureArrays.map((featureArray, index) => (
+          <Grid item xs={4} key={index}>
+            <Typography>
+              {featureNames[index]}:{" "}
+              {averageFeatureValue(featureArray).toFixed(3)}
+            </Typography>
           </Grid>
         ))}
+        <Grid item xs={4}>
+          <Typography>
+            {featureNames[7]}: {averageFeatureValue(allTempo).toFixed(0)} bpm
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography>
+            {featureNames[8]}:{" "}
+            {convertToMinutes(averageFeatureValue(allDuration))} minutes
+          </Typography>
+        </Grid>
       </Grid>
     </Box>
   );
